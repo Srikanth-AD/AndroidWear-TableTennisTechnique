5bb141b4c7866624b66a1309225200c9c0b9f4cd
Srikanth AD <adsrikanth@gmail.com>
2018-04-27 21:42:47 -0500

    use 1 listener instead of individual listeners per sensor and updated vibration method to improve default amplitude/vibration intensity

--- wear/src/main/java/me/srikanth/myapplication/MainActivity.java
+++ wear/src/main/java/me/srikanth/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Vibrator;
 import android.support.wear.widget.BoxInsetLayout;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
+import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,7 +22,6 @@ public class MainActivity extends WearableActivity {
     private Sensor mGravitySensor;
     private Sensor mAccelerometerSensor;
     public SensorEventListener _SensorEventListener;
-    public SensorEventListener _SensorEventListener1;
     private String gravityValue = "";
     TextView gravityValueTextView;
     public String startBackhandPracticeBtnMode = "start";
@@ -42,12 +42,11 @@ public class MainActivity extends WearableActivity {
                 if (startBackhandPracticeBtnMode.equals("start")) {
                     gravityValueTextView.setText("");
                     mSensorManager.registerListener(_SensorEventListener, mGravitySensor, 500000);
-                    mSensorManager.registerListener(_SensorEventListener1, mAccelerometerSensor, 500000);
+                    mSensorManager.registerListener(_SensorEventListener, mAccelerometerSensor, 500000);
                     startBackhandPracticeBtnMode = "stop";
                     startBackhandPracticeButton.setText("Stop");
                 } else {
                     mSensorManager.unregisterListener(_SensorEventListener);
-                    mSensorManager.unregisterListener(_SensorEventListener1);
                     forwardCount = 0; // reset counters
                     rescueCount = 0;
                     startBackhandPracticeBtnMode = "start";
@@ -69,7 +68,7 @@ public class MainActivity extends WearableActivity {
         _SensorEventListener =  new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                //Log.d(TAG, event.sensor.getType() + "");
+
                 if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
 
                     if (! (event.values[0] + "").equals(gravityValue)) {
@@ -80,8 +79,24 @@ public class MainActivity extends WearableActivity {
                         //gravityValueTextView.setText(gravityValue);
                         //Log.d(TAG, gravityValue);
                     }
-
                 }
+
+                else if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+                    //Log.d(TAG, Math.abs(event.values[0]) + " " + Math.abs(event.values[1]) +  " g: " + gravityValue);
+                    if (Math.abs(event.values[1]) > 7 && (Double.parseDouble(gravityValue) > 4.4)) {
+                        //Log.d(TAG, "g: " + gravityValue + " " + " accelerate Y axis: " + " " + event.values[1]);
+                        rescueCount++;
+                        triggerVibration();
+                        parentLayout.setBackgroundColor(Color.RED);
+                    }
+
+                    if (Math.abs(event.values[0]) > 7 && (Double.parseDouble(gravityValue) < 4.4)) {
+                        forwardCount++;
+                    }
+
+                    gravityValueTextView.setText("Forward: " + forwardCount + " Rescue: " + rescueCount);
+                }
+
                 else
                     Log.d(TAG, "Unknown sensor type");
             }
@@ -91,42 +106,11 @@ public class MainActivity extends WearableActivity {
                 Log.d(TAG, accuracy + "");
             }
         };
-
-        _SensorEventListener1 =  new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-                    //Log.d(TAG, Math.abs(event.values[0]) + " " + Math.abs(event.values[1]) +  " g: " + gravityValue);
-
-                    if (Math.abs(event.values[1]) > 7 && (Double.parseDouble(gravityValue) > 4.6)) {
-                        //Log.d(TAG, "g: " + gravityValue + " " + " accelerate Y axis: " + " " + event.values[1]);
-                        rescueCount++;
-                        triggerVibration();
-                        parentLayout.setBackgroundColor(Color.RED);
-                    }
-
-                    if (Math.abs(event.values[0]) > 7 && (Double.parseDouble(gravityValue) < 4.6)) {
-                        forwardCount++;
-                    }
-
-                    gravityValueTextView.setText("Forward: " + forwardCount + " Rescue: " + rescueCount);
-                }
-                else
-                    Log.d(TAG, "Unknown sensor type");
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
     }
 
     public void triggerVibration() {
         Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        long[] vibrationPattern = {0, 100};
-        final int indexInPatternToRepeat = -1;
-        vibrator.vibrate(vibrationPattern, indexInPatternToRepeat);
+        vibrator.vibrate(150);
     }
 
     @Override
@@ -138,6 +122,5 @@ public class MainActivity extends WearableActivity {
     protected void onStop() {
         super.onStop();
         mSensorManager.unregisterListener(_SensorEventListener);
-        mSensorManager.unregisterListener(_SensorEventListener1);
     }
 }
