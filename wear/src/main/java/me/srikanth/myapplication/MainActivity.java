5a01a98482e35a62c35c44eccc9c1cbceff63778
Srikanth AD <adsrikanth@gmail.com>
2018-05-05 23:21:35 -0500

    Updated logic to accurately count: forward and upward movements. One bug which skips a forward rep. immediately following a rescue rep. is to be resolved

--- wear/src/main/java/me/srikanth/myapplication/MainActivity.java
+++ wear/src/main/java/me/srikanth/myapplication/MainActivity.java
@@ -23,12 +23,17 @@ public class MainActivity extends WearableActivity {
     public String startBackhandPracticeBtnMode = "start";
     Button startBackhandPracticeButton;
     int forwardCount, rescueCount = 0;
-    double gravityXPeakValue = 0;
     int accelerationPeakValue = 0;
+    float gravityPeak = 0.0f;
+    private long mLastTime = 0;
+    private long accelerationPeakTimestamp = 0;
+    private boolean mUp = false;
 
-    private static final float GRAVITY_THRESHOLD = 6.0f; // to differentiate forward versus upward movement
-    private static final int MIN_LINEAR_ACCELERATION_AT_PEAK = 18; // minimum acceptable peak acceleration during a rep
-    private  static final int MAX_LINEAR_ACCELERATION_AT_REST = 2;  // due to normal hand movement, acceleration may never be zero
+    private static final float GRAVITY_THRESHOLD = 6.5f; // to differentiate forward versus upward movement
+    private static final int MIN_LINEAR_ACCELERATION_AT_PEAK = 15; // minimum acceptable peak acceleration during a rep
+    private  static final int MAX_LINEAR_ACCELERATION_AT_REST = 3;  // due to normal hand movement, acceleration may never be zero
+    private static final long TIME_THRESHOLD_NS = 2000000000; // in nanoseconds (= 2sec)
+    private static final int VIBRATE_FOR_MS = 250; // in milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +54,9 @@ public class MainActivity extends WearableActivity {
 
                     // reset counters
                     resetCountsPerSession();
-                    resetCountsPerRepetition();
+                    resetCountsPerRep();
 
-                    sessionResultsTextView.setText("");
+                    sessionResultsTextView.setText(" Forward: 0 Rescue: 0");
 
                     if (mGravitySensor != null)
                         mSensorManager.registerListener(_SensorEventListener,
@@ -82,48 +87,45 @@ public class MainActivity extends WearableActivity {
             @Override
             public void onSensorChanged(SensorEvent event) {
 
-                if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
-
-                    if (event.values[0] > gravityXPeakValue)
-                        gravityXPeakValue = event.values[0];
-                }
+                mLastTime = event.timestamp;
 
                 if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
 
-                    if (Math.abs(event.values[0]) > accelerationPeakValue)
-                        accelerationPeakValue = (int) Math.abs(event.values[0]);
-
-                    // Forward rep. and reset
-                    if (Math.abs(event.values[0]) <= MAX_LINEAR_ACCELERATION_AT_REST
-                            && accelerationPeakValue > MIN_LINEAR_ACCELERATION_AT_PEAK
-                            && gravityXPeakValue < GRAVITY_THRESHOLD) {
-
-                        forwardCount++;
-                        Log.d(TAG, "Gravity X Peak: " + gravityXPeakValue);
-                        Log.d(TAG, "Forward count: " + forwardCount);
-                        resetCountsPerRepetition();
-
-                        sessionResultsTextView.setText("Forward: "
-                                .concat(String.valueOf(forwardCount))
-                                .concat(" Rescue: ")
-                                .concat(String.valueOf(rescueCount)));
-
+                    if (event.timestamp - accelerationPeakTimestamp > TIME_THRESHOLD_NS) {
+                        accelerationPeakValue = 0; // acceleration peak & gravity peak are no longer valid
+                        gravityPeak = 0.0f;
                     }
 
-                    if (Math.abs(event.values[0]) < MAX_LINEAR_ACCELERATION_AT_REST &&
+                    // Forward movement
+                    if (Math.abs(event.values[0]) > accelerationPeakValue) {
+                        accelerationPeakValue = (int) Math.abs(event.values[0]);
+                        accelerationPeakTimestamp = event.timestamp;
+                    }
+
+                    if (event.timestamp - accelerationPeakTimestamp < TIME_THRESHOLD_NS &&
+                            Math.abs(event.values[0]) <= MAX_LINEAR_ACCELERATION_AT_REST &&
                             accelerationPeakValue > MIN_LINEAR_ACCELERATION_AT_PEAK &&
-                            gravityXPeakValue > GRAVITY_THRESHOLD) {
+                            gravityPeak < GRAVITY_THRESHOLD) {
+                        forwardCount++;
+                        resetCountsPerRep();
+                        sessionResultsTextView.setText(" Forward: " + String.valueOf(forwardCount) + " Rescue: " + String.valueOf(rescueCount));
+                    }
+                }
 
-                        rescueCount++;
-                        Log.d(TAG, "Gravity X Peak: " + gravityXPeakValue);
-                        Log.d(TAG, "Rescue count: " + rescueCount);
-                        resetCountsPerRepetition();
-                        triggerVibration();
+                if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
 
-                        sessionResultsTextView.setText("Forward: "
-                                .concat(String.valueOf(forwardCount))
-                                .concat(" Rescue: ")
-                                .concat(String.valueOf(rescueCount)));
+                    if (Math.abs(event.values[0]) > gravityPeak) {
+                        gravityPeak = Math.abs(event.values[0]);
+                    }
+
+                    // Upward and downward movement
+                    if ((Math.abs(event.values[0]) > GRAVITY_THRESHOLD)) {
+
+                        if (event.timestamp - mLastTime < TIME_THRESHOLD_NS &&
+                                mUp != (event.values[0] > 0)) {
+                            onStrokeDetected(!mUp, accelerationPeakValue);
+                        }
+                        mUp = event.values[0] > 0;
                     }
                 }
             }
@@ -134,9 +136,24 @@ public class MainActivity extends WearableActivity {
         };
     }
 
-    public void resetCountsPerRepetition() {
-        gravityXPeakValue = 0;
+    /**
+     * Called on detection of a successful down -> up or up -> down movement of hand.
+     */
+    private void onStrokeDetected(boolean up, int accelerationPeak) {
+        // we only count a pair of up and down as one successful movement
+        // check for acceleration peak helps avoid simple up/down hand movement without any acceleration
+        if (up || accelerationPeak < MIN_LINEAR_ACCELERATION_AT_PEAK) {
+            return;
+        }
+        rescueCount++;
+        triggerVibration();
+        sessionResultsTextView.setText(" Forward: " + String.valueOf(forwardCount) + " Rescue: " + String.valueOf(rescueCount));
+        resetCountsPerRep();
+    }
+
+    public void resetCountsPerRep() {
         accelerationPeakValue = 0;
+        gravityPeak = 0.0f;
     }
 
     public void resetCountsPerSession() {
@@ -147,7 +164,7 @@ public class MainActivity extends WearableActivity {
     public void triggerVibration() {
         Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         if (vibrator != null) {
-            vibrator.vibrate(135);
+            vibrator.vibrate(VIBRATE_FOR_MS);
         }
     }
 
