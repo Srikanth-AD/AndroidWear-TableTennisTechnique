9e88b6a710c64d8ee271864dd14e233d9e19a4fa
Srikanth AD <srikanth@loxabeauty.com>
2018-05-18 15:07:00 -0500

    removed an unused import

--- wear/src/main/java/me/srikanth/myapplication/ForehandDrive.java
+++ wear/src/main/java/me/srikanth/myapplication/ForehandDrive.java
@@ -2,7 +2,6 @@ package me.srikanth.myapplication;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.wearable.activity.WearableActivity;
 
 public class ForehandDrive extends FragmentActivity {
 
