b0aba325f65daa62436161bee6725fe66f1e8950
Srikanth AD <adsrikanth@gmail.com>
2018-05-21 01:11:00 -0500

    added MainActivity's listview to a relative layout to set full screen background color

--- wear/src/main/java/me/srikanth/myapplication/MainActivity.java
+++ wear/src/main/java/me/srikanth/myapplication/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listview);
+        setContentView(R.layout.layout_main);
 
         final ListView listview = findViewById(R.id.listview);
         Resources res = getResources();
