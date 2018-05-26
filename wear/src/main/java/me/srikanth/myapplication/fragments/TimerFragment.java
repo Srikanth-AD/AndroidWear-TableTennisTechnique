468996af45f4c5c76131aed5f93835b0eb31feee
Srikanth AD <adsrikanth@gmail.com>
2018-05-26 14:45:42 -0500

    added UI changes to include icons for timer fragment and used dimensions xml to set consistent font sizes

--- wear/src/main/java/me/srikanth/myapplication/fragments/TimerFragment.java
+++ wear/src/main/java/me/srikanth/myapplication/fragments/TimerFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import me.srikanth.myapplication.R;
 import me.srikanth.myapplication.models.SharedViewModel;
@@ -29,18 +31,26 @@ public class TimerFragment extends Fragment {
         model = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
         model.getCurrentTimerMode().setValue(TIMER_MODE_INACTIVE);
 
-        final Button startBtn = view.findViewById(R.id.btn_start);
-        final Button pauseBtn = view.findViewById(R.id.btn_pause);
-        final Button resumeBtn = view.findViewById(R.id.btn_resume);
-        final Button stopBtn = view.findViewById(R.id.btn_stop);
+        final ImageButton startBtn = view.findViewById(R.id.btn_start);
+        final ImageButton pauseBtn = view.findViewById(R.id.btn_pause);
+        final ImageButton resumeBtn = view.findViewById(R.id.btn_resume);
+        final ImageButton stopBtn = view.findViewById(R.id.btn_stop);
+        final TextView startBtnLabel = view.findViewById(R.id.label_btn_start);
+        final TextView pauseBtnLabel = view.findViewById(R.id.label_btn_pause);
+        final TextView resumeBtnLabel = view.findViewById(R.id.label_btn_resume);
+        final TextView stopBtnLabel = view.findViewById(R.id.label_btn_stop);
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 model.getStartTime().setValue(System.currentTimeMillis());
                 model.getCurrentTimerMode().setValue(TIMER_MODE_STARTED);
+
                 startBtn.setVisibility(View.GONE);
+                startBtnLabel.setVisibility(View.GONE);
+
                 pauseBtn.setVisibility(View.VISIBLE);
+                pauseBtnLabel.setVisibility(View.VISIBLE);
             }
         });
 
@@ -48,9 +58,15 @@ public class TimerFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 model.getCurrentTimerMode().setValue(TIMER_MODE_PAUSED);
+
                 pauseBtn.setVisibility(View.GONE);
+                pauseBtnLabel.setVisibility(View.GONE);
+
                 resumeBtn.setVisibility(View.VISIBLE);
+                resumeBtnLabel.setVisibility(View.VISIBLE);
+
                 stopBtn.setVisibility(View.VISIBLE);
+                stopBtnLabel.setVisibility(View.VISIBLE);
             }
         });
 
@@ -58,9 +74,15 @@ public class TimerFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 model.getCurrentTimerMode().setValue(TIMER_MODE_RESUMED);
+
                 resumeBtn.setVisibility(View.GONE);
+                resumeBtnLabel.setVisibility(View.GONE);
+
                 stopBtn.setVisibility(View.GONE);
+                stopBtnLabel.setVisibility(View.GONE);
+
                 pauseBtn.setVisibility(View.VISIBLE);
+                pauseBtnLabel.setVisibility(View.VISIBLE);
             }
         });
 
@@ -69,9 +91,15 @@ public class TimerFragment extends Fragment {
             public void onClick(View view) {
                 model.getStopTime().setValue(System.currentTimeMillis());
                 model.getCurrentTimerMode().setValue(TIMER_MODE_STOPPED);
+
                 resumeBtn.setVisibility(View.GONE);
+                resumeBtnLabel.setVisibility(View.GONE);
+
                 stopBtn.setVisibility(View.GONE);
+                stopBtnLabel.setVisibility(View.GONE);
+
                 startBtn.setVisibility(View.VISIBLE);
+                startBtnLabel.setVisibility(View.VISIBLE);
             }
         });
 
